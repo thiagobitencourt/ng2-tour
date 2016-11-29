ad737e439ad1061cb36326309cd646b84da427f3
Thiago Bitencourt <thiago.bitencourt@senior.com.br>
2016-11-29 13:18:56 -0200

    Bindings objects

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <div class="jumbotron">
-      <h1>Welcome to our App!</h1>
-    </div>
+
+    <header> 
+      <nav class="navbar navbar-inverse">
+        <div>
+          <a href="/" class="navbar-brand">My Angular 2 App!</a>
+        </div>
+      </nav>
+    </header>
+
+    <main>
+      <div class="jumbotron">
+        <h1>Welcome to our App!</h1>
+        <p>{{message}}</p>
+      </div>
+
+      <div *ngIf="users">
+        <div *ngFor="let user of users">
+          <p>The user is: {{user.name}} ({{user.username}}).</p>
+        </div>
+      </div>
+    </main>
+
+    <footer class="text-center">
+      Copyright &copy; 2016
+    </footer>
   `,
   styles: [
     `.jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2) } `
   ]
 })
 
-export class AppComponent {}
+export class AppComponent {
+  message = "Hello!";
+  users = [
+    { id: 25, name: 'Chris', username: 'sevilayha'},
+    { id: 26, name: 'Nick', username: 'whatnicktweets'},
+    { id: 27, name: 'Holly', username: 'hollylawly'}
+  ];
+}
