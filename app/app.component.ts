a0bc90984d618ac4b719f6d345489004ad06eccb
Thiago Bitencourt <thiago.bitencourt@senior.com.br>
2016-11-30 13:24:20 -0200

    Separete html file and add current user

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,48 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-
-    <header> 
-      <nav class="navbar navbar-inverse">
-        <div>
-          <a href="/" class="navbar-brand">My Angular 2 App!</a>
-        </div>
-      </nav>
-    </header>
-
-    <main>
-
-      <div class="row">
-
-        <div class="col-sm-4">
-          <div *ngIf="users">
-            <ul class="list-group users-list">
-              <li class="list-group-item"
-                *ngFor="let user of users">
-                {{user.name}} ({{user.username}})
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="col-sm-8">
-          <div class="jumbotron">
-            <h1>Welcome to our App!</h1>
-            <p>{{message}}</p>
-          </div>
-        </div>
-
-      </div>
-
-    </main>
-
-    <footer class="text-center">
-      Copyright &copy; 2016
-    </footer>
-  `,
+  templateUrl: 'app/app.component.html',
   styles: [
-    `.jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2) } `
+    `
+      .users-list li {
+        cursor: pointer;
+      }
+      .jumbotron .glyphicon {
+        font-size: 80px;
+      }
+    `
   ]
 })
 
@@ -54,4 +22,10 @@ export class AppComponent {
     { id: 26, name: 'Nick', username: 'whatnicktweets'},
     { id: 27, name: 'Holly', username: 'hollylawly'}
   ];
+  activeUser;
+
+  selectUser(user) {
+    this.activeUser = user;
+    console.log(this.activeUser);
+  }
 }
