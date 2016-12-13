334007c215c31f6c19282ab7d51ae2e4f056481d
Thiago Bitencourt <thiago.bitencourt@senior.com.br>
2016-12-13 17:20:09 -0200

    Add CRUD funcionality with in-memory-web-api

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,12 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { RouterModule }   from '@angular/router';
+import { HttpModule }   from '@angular/http';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
@@ -9,12 +15,13 @@ import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
 
-import { AppRoutingModule }     from './app-routing.module'
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [ 
