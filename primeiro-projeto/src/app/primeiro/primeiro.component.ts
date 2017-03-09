86d7dfff02d42e6ec08f02c16f257c25cd6618f3
Thiago Bitencourt <thiago.bitencourt@senior.com.br>
2017-03-09 07:42:35 -0300

    First initial components by hands and generated by cli

---
+++ primeiro-projeto/src/app/primeiro/primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'primeiro-component',
+  template: 
+  `
+    <p>Meu primeiro component com Angular 2!</p>
+  `
+})
+export class PrimeiroComponent{ }
