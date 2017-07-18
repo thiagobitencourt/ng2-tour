1f789b0080c60c94569792819b3e3227c2ff3625
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-18 20:10:11 -0300

    Lear DOM access with ViewChild

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -11,14 +11,16 @@ export class OutputPropertyComponent implements OnInit {
   @Input() valor: number = 0;
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput: ElementRef;
+
   incrementa() {
-    this.valor++;
-    this.mudouValor.emit({ novoValor: this.valor });
+    this.campoValorInput.nativeElement.value++;
+    this.mudouValor.emit({ novoValor: this.campoValorInput.nativeElement.value });
   }
 
   decrementa() {
-    this.valor--;
-    this.mudouValor.emit({ novoValor: this.valor });
+    this.campoValorInput.nativeElement.value--;
+    this.mudouValor.emit({ novoValor: this.campoValorInput.nativeElement.value });
   }
 
   constructor() { }
