288e1619296a36c6562f8393d9d0d0433ab39cd2
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-17 23:21:16 -0300

    Learn two-way data binding with ngModel

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
     DataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MeuFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
