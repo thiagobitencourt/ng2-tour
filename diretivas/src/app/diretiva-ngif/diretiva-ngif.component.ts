91103d22b48347e2ffd60959a663fb22f97fe504
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-18 23:11:50 -0300

    learn ngif directive and attribute hidden as alteranative

---
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css']
+})
+export class DiretivaNgifComponent implements OnInit {
+
+  cursos: string[] = ["Angular 2"];
+  mostrarCursos: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
