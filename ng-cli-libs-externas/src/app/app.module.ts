cea54ed250a0bc287bca6b7d3f38855311620f72
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-18 22:25:17 -0300

    Install materialize css

--- ng-cli-libs-externas/src/app/app.module.ts
+++ ng-cli-libs-externas/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterializeModule } from "angular2-materialize";
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
