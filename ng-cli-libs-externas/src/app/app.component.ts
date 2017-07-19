55f701388144dccc4f9312739727f336e250c4fd
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-18 22:31:21 -0300

    importing lodash

--- ng-cli-libs-externas/src/app/app.component.ts
+++ ng-cli-libs-externas/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  list = _.map([1, 2, 3], n => `# ${n}`);
 }
