797046747e19c9b24f6f8b73273ac2041464e748
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-19 20:41:31 -0300

    Learn templates and directives without *

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
-  cursos: string[] = ["Angular 2"];
+  cursos: string[] = [ 'Angular', 'Java', 'Phonegap' ];
   mostrarCursos: boolean = false;
 
   constructor() { }
