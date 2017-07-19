4b1e31a0ae4f7349f37f545f7f27d05e7b99a62b
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-19 20:57:00 -0300

    Learn ngClass directive

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretiveNgswitchComponent } from './diretive-ngswitch/diretive-ngswitch.component';
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     DiretiveNgswitchComponent,
-    DiretivaNgforComponent
+    DiretivaNgforComponent,
+    DiretivaNgclassComponent
   ],
   imports: [
     BrowserModule
