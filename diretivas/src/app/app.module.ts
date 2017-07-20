a59d73442a54e9e542054e3e925ed3a3a9c4539f
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-19 21:33:00 -0300

    Learn elvis operator

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DiretiveNgswitchComponent } from './diretive-ngswitch/diretive-ngswitch
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
+import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.co
     DiretiveNgswitchComponent,
     DiretivaNgforComponent,
     DiretivaNgclassComponent,
-    DiretivaNgstyleComponent
+    DiretivaNgstyleComponent,
+    OperadorElvisComponent
   ],
   imports: [
     BrowserModule,
