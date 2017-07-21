3418049a63580d30dc3713a4c0bd7f5e212ecf95
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-21 20:29:46 -0300

    Create a start app for learning routes

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { MaterializeModule } from 'angular2-materialize';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterializeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
