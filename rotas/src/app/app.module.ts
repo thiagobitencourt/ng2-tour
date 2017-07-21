45f8d362bcabb908b3cf4d9915b1b1e3e9ce04da
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-21 20:44:00 -0300

    Create the first route

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 
+import { routing } from './app.routing';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +19,8 @@ import { CursosComponent } from './cursos/cursos.component';
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
