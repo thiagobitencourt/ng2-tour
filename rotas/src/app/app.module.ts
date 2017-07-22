258dc339a47efa024b86523b239fba4d21096b7e
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-21 21:16:55 -0300

    Creat a route with params and obtein the params inside the component

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 
 import { routing } from './app.routing';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -2,11 +2,13 @@ import { ModuleWithProviders} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 const APP_ROUTES: Routes = [
   { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent }
 ];
