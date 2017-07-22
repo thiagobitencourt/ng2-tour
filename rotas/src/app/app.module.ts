a47023a6c53438f297919705eafbadf04e20b792
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-22 15:21:48 -0300

    Add lazy loading on routes

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { MaterializeModule } from 'angular2-materialize';
 
-import { CursosModule } from './cursos/cursos.module';
-import { AlunosModule } from './alunos/alunos.module';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -20,8 +17,6 @@ import { AppRoutingModule } from './app.routing.module';
   imports: [
     BrowserModule,
     MaterializeModule,
-    CursosModule,
-    AlunosModule,
     AppRoutingModule
   ],
   providers: [],
