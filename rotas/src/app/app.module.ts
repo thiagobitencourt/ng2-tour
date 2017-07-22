b3086b86944b153dc31ffe548a206b97318269f0
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-21 22:09:28 -0300

    Refactor to configure routes in a module

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -8,11 +8,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 
-import { routing } from './app.routing';
+// import { routing } from './app.routing';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
   imports: [
     BrowserModule,
     MaterializeModule,
-    routing
+    // routing
+    AppRoutingModule
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
