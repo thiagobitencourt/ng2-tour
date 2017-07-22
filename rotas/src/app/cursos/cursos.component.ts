dd4f035c89d8c929eec2e2820d42e414770595e5
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-21 21:59:11 -0300

    Learn query params on routes

--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 import { CursosService } from './cursos.service';
 
@@ -10,11 +12,30 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  proximaPagina() {
+    this.router.navigate(['/cursos'], { queryParams: { pagina: ++this.pagina }})
+  }
 
   ngOnInit() {
     this.cursos = this.cursosService.getCursos();
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      });
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
