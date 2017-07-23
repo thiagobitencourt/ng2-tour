d9b7f81e69dba95cb824863d4892b9f1692a2991
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-23 16:49:47 -0300

    Creat a complete form

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
-  usuario: any = {
-
-  }
+  usuario: any = { }
 
   constructor() { }
 
