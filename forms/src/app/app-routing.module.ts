528c2db90714e6d84558246023e396ec6bd1db93
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-23 22:28:23 -0300

    Create data-form module

--- forms/src/app/app-routing.module.ts
+++ forms/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'templateForm'
+    redirectTo: 'dataForm'
   },
   {
     path: 'templateForm',
--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -6,15 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
-import { DataFormComponent } from './data-form/data-form.component';
 import { FormDebugComponent } from './form-debug/form-debug.component';
 import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
+import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateFormComponent,
-    DataFormComponent,
     FormDebugComponent,
     CampoControlErroComponent
   ],
@@ -22,6 +21,7 @@ import { CampoControlErroComponent } from './campo-control-erro/campo-control-er
     BrowserModule,
     FormsModule,
     HttpModule,
+    DataFormModule,
     AppRoutingModule
   ],
   providers: [],
