3f4168c921dc08aa4a0d33018c1b25d8a0843437
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-23 21:33:00 -0300

    Obtain address data from server based on CEP

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { CampoControlErroComponent } from './campo-control-erro/campo-control-er
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   providers: [],
--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-template-form',
@@ -9,7 +11,7 @@ export class TemplateFormComponent implements OnInit {
 
   usuario: any = { }
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
@@ -26,6 +28,20 @@ export class TemplateFormComponent implements OnInit {
     return {
       'has-error': this.verificaValidTouched(campo),
       'has-feedback': this.verificaValidTouched(campo)
+    }
+  }
+
+  consultaCep(cep) {
+    cep = cep.replace(/\D/g, '');
+    if(cep != '') {
+      let validaCep = /^[0-9]{8}$/;
+      if(validaCep.test(cep)) {
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .map(dados => dados.json())
+          .subscribe(dados => {
+            console.log(dados);
+          });
+      }
     }
   }
 
