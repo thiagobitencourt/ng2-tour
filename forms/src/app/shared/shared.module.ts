0ad9a562af125a4525d07feec3b8f13f2bcb3792
Thiago Bitencourt <thiago.bitencourt@senior.com.br>
2017-07-26 07:05:13 -0300

    Create a service to obtain states

--- forms/src/app/shared/shared.module.ts
+++ forms/src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
 import { FormDebugComponent } from './form-debug/form-debug.component';
 import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
+import { DropdownService } from './services/dropdown.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpModule
   ],
   declarations: [
     FormDebugComponent,
@@ -15,6 +18,7 @@ import { CampoControlErroComponent } from './campo-control-erro/campo-control-er
   exports: [
     FormDebugComponent,
     CampoControlErroComponent
-  ]
+  ],
+  providers: [ DropdownService ]
 })
 export class SharedModule { }
